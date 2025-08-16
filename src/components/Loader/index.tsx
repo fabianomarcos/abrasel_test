c77d1a68c19f4c4e8373c3593529bf38cb745e7f
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-16 17:09:12 -0300

    feat: loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+export const Loader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-500 opacity-40">
+      <div
+        className="
+          animate-spin rounded-full border-t-transparent
+          h-10 w-10 border-4 border-emerald-400
+        "
+      />
+    </div>
+  )
+}
