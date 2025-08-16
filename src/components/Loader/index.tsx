486d602833bbf1d18f8200294f62ee071ca02f6b
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-16 17:13:49 -0300

    fix: z index loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-500 opacity-40">
+    <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-500 opacity-40 z-10">
       <div
         className="
           animate-spin rounded-full border-t-transparent
