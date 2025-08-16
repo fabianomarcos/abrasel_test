cf0f03c7bb519a319f8476467ff2c7ca2fdaa994
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-16 15:45:04 -0300

    feat: button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import { ButtonHTMLAttributes } from 'react'
+
+export interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean
+}
+
+export const Button = ({ children, loading = false, ...rest }: IProps) => {
+  return (
+    <button
+      className="
+          flex items-center justify-center gap-4 px-3 rounded-md font-semibold
+          text-md cursor-pointer transition-opacity hover:opacity-80
+          bg-green-600
+          h-10
+          max-w-[260px]
+        "
+      {...rest}
+    >
+      {loading ? 'Carregando...' : children}
+    </button>
+  )
+}
