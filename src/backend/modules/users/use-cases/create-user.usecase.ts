75c5af55537537396be434d775c4456dd7066182
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-16 22:23:19 -0300

    feat: back - create user use case

---
+++ src/backend/modules/users/use-cases/create-user.usecase.ts
@@ -0,0 +1,30 @@
+import { HashGenerator } from '@/backend/cryptography/contracts/hash-generator'
+import { User, UserOrm } from '../entities/user.entity'
+import { IUserRepository } from '../repositories/IUserRepository'
+import { BadRequestError } from '@/backend/errors/bad-request'
+import { validatePassword } from '@/backend/pipes/validate-password'
+
+export class CreateUserUseCase {
+  constructor(
+    private readonly usersRepository: IUserRepository,
+    private hashGeneration: HashGenerator,
+  ) {}
+
+  async execute(input: UserOrm): Promise<User> {
+    const existingUser = await this.usersRepository.listByEmail(input.email)
+    if (existingUser) throw new BadRequestError('User already exists')
+
+    validatePassword(input.password)
+
+    const hashedPassword = await this.hashGeneration.hash(input.password)
+
+    const response = await this.usersRepository.create({
+      ...input,
+      password: hashedPassword,
+    })
+
+    const user = { ...response, password: null } as User
+
+    return user
+  }
+}
