70982156b6eecee6cf5318e59402b88b6e3ae073
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-16 22:24:49 -0300

    feat: routes

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+const createPageUrl = (base: string, sub_base: string = '') =>
+  `/${base}/${sub_base}`
+
+export const ROUTES = {
+  PRIVATE: {
+    HOME: '/',
+  },
+
+  PUBLIC: {
+    LOGIN: createPageUrl('sign-in'),
+    REGISTER: createPageUrl('sign-up'),
+    FORGOT_PASSWORD: createPageUrl('forgot-password'),
+  },
+}
