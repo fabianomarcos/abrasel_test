ed15872359b495c802b12d65cb7d7779579a75b0
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-18 06:56:18 -0300

    feat: profile hooks

---
+++ src/app/(private)/profile/hooks/use-get-cep.ts
@@ -0,0 +1,47 @@
+import zipCodeApi from '@/services/zip-code-api'
+import { useCallback, useEffect, useState } from 'react'
+
+export interface ICep {
+  cep: string
+  state: string
+  city: string
+  neighborhood: string
+  street: string
+  service: string
+}
+
+export const useGetCep = (cep: string) => {
+  console.log('cep: ', cep)
+  const [loading, setLoading] = useState(false)
+  const [dataCep, setDataCep] = useState({} as ICep)
+  const [blockFields, setBlockFields] = useState(false)
+
+  const getCep = useCallback(async () => {
+    if (!cep) return
+
+    try {
+      setLoading(true)
+      const { data } = await zipCodeApi.get<ICep>(`/${cep}`)
+      setBlockFields(!!data.city)
+      setDataCep(data)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.error('error: ', error)
+      alert(
+        'Ocorreu algum erro ao buscar o cep, por favor, preencha manualmente.',
+      )
+    }
+  }, [cep])
+
+  useEffect(() => {
+    if (!loading) getCep()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getCep])
+
+  return {
+    dataCep,
+    blockFields,
+    loading,
+  }
+}
