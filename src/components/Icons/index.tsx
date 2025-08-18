c6e7cc6616fb89ed93d01a96bdbe62e57ddc9ac8
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-18 00:27:50 -0300

    feat: new icons

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,3 +1,13 @@
-import { FiInfo as Info } from 'react-icons/fi'
+import { IoEyeOutline as EyeOutline } from 'react-icons/io5'
+import { FaLinkedin as Linkedin } from 'react-icons/fa6'
 
-export { Info }
+import {
+  FiInfo as Info,
+  FiLogOut as LogOut,
+  FiUser as User,
+  FiUsers as Users,
+} from 'react-icons/fi'
+
+import { GiBullseye as Target } from 'react-icons/gi'
+
+export { EyeOutline, Info, Linkedin, LogOut, User, Users, Target }
