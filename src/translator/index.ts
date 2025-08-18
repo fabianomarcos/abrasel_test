a81a5a320b4e17772fd1417d3dfbb1f44d464bd3
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-18 00:28:58 -0300

    feat: translator

---
+++ src/translator/index.ts
@@ -0,0 +1,30 @@
+export type LanguageType = 'es-US' | 'pt-BR'
+
+export const ERRORS = (text?: string) => {
+  const language = navigator.language as LanguageType
+
+  const error = {
+    'Some fields are invalids:': {
+      'en-US': 'Some fields are invalids',
+      'pt-BR': 'Alguns campos são inválidos',
+    },
+    'Credential are not valid.': {
+      'en-US': 'Credential are not valid.',
+      'pt-BR': 'Credenciais Inválidas',
+    },
+
+    default: {
+      'en-US': 'An error occurred, please try later.',
+      'pt-BR': 'Ocorreu algum erro, tente mais tarde',
+    },
+  }
+
+  const textKey = (text as keyof typeof error) || 'default'
+  const errorWithKey = error[textKey || 'default'] || error.default
+  const languageKey = language as keyof typeof errorWithKey
+  const message =
+    errorWithKey[languageKey] ||
+    'Ocorreu algum erro tente novamente mais tarde.'
+
+  return message
+}
