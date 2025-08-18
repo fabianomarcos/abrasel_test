3f36362e222e1a26e3d1b701abb8fb47a070f18d
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-18 00:26:43 -0300

    feat: header hooks

---
+++ src/components/Header/hooks/use-data-header.ts
@@ -0,0 +1,30 @@
+'use client'
+import { useState } from 'react'
+
+export function useDataHeader() {
+  const [showOptions, setShowOptions] = useState(false)
+  console.log('showOptions: ', showOptions)
+
+  const disappear = (styles: string) => {
+    const element = document.querySelector(
+      '[data-id="animation"]',
+    ) as HTMLDivElement
+    if (element) {
+      element.classList.add(styles)
+      element.addEventListener('animationend', () => {
+        element.style.display = 'none'
+      })
+    }
+  }
+
+  const handleToggleUserOptions = (styles: string) => {
+    const timer = showOptions ? 1000 : 0
+    setTimeout(() => setShowOptions(!showOptions), timer)
+    disappear(styles)
+  }
+
+  return {
+    showOptions,
+    handleToggleUserOptions,
+  }
+}
--- src/components/Header/hooks/use-validate-token.ts
+++ src/components/Header/hooks/use-validate-token.ts
@@ -1,3 +1,4 @@
+'use client'
 import jwt from 'jsonwebtoken'
 import { useCallback } from 'react'
 import { useRouter } from 'next/navigation'
