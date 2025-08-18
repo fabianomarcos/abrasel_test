8d4a2230d35335d9a1ddf55feb69190deea3cffc
Fabiano Marcos <fabianofmarcos@gmail.com>
2025-08-18 15:10:50 -0300

    fix: delete user

--- src/backend/modules/users/controllers/delete-user.controller.ts
+++ src/backend/modules/users/controllers/delete-user.controller.ts
@@ -7,11 +7,10 @@ class Controller {
   constructor(private deleteUserUsecase: DeleteUserUseCase) {}
 
   async delete(request: NextRequest): Promise<NextResponse> {
-    const body = await request.json()
     const id = request.nextUrl.pathname.split('/').pop()
     if (!id) throw new BadRequestError('User id is required')
-    const user = await this.deleteUserUsecase.execute({ ...body, id })
-    return NextResponse.json({ user })
+    await this.deleteUserUsecase.execute(id)
+    return new NextResponse(null, { status: 204 })
   }
 }
 
